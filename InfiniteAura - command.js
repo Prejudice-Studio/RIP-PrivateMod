84b05964dc756940d50e599bf94af6ffeebd7dd9
墨缘 <MoYuanCN@gmail.com>
2023-12-03 21:40:24 +0800

    Update InfiniteAura - command.js
    集成setting进主js
    增加原作者与项目地址标注

--- InfiniteAura - command.js
+++ InfiniteAura - command.js
@@ -168,5 +168,51 @@ function onExecuteCommandEvent(command) {
     return true;
 }
 
+// 配置界面
+function InfiniteAuraSetting() {
+    const custom_form = `
+    {
+    "type": "custom_form",
+    "title": "配置界面",
+    "content": [
+      {
+        "type": "input",
+        "text": "攻击次数",
+        "placeholder": "默认为3"
+      },
+      {
+        "type": "input",
+        "text": "最大攻击距离",
+        "placeholder": "默认为500"
+      },
+      {
+        "type": "input",
+        "text": "自动攻击间隔(单位tick)",
+        "placeholder": "默认为15"
+      },
+      {
+        "type": "input",
+        "text": "Pos模式最高Y坐标",
+        "placeholder": "默认为83"
+      }
+    ]
+    }
+    `;
+    addForm(custom_form, function (...args) {
+        executeCommand("/Set ATTACK_COUNT" + args[0]),executeCommand("/Set MAX_RANGE " + args[1]),executeCommand("/Set AUTO_INTERVAL " + args[2]),executeCommand("/Set POS_Y_MAX " + args[3])
+        clientMessage("攻击次数为" + args[0] , "最大攻击距离为" + args[1] , "自动攻击间隔为" + args[2] , "Pos模式最高Y坐标为" + args[3] ,  "设置完毕")
+    })
+}
+
+function onSendChatMessageEvent(message) {
+    if (message.toLowerCase() === 'set') {
+        InfiniteAuraSetting();
+        return true;  // 阻止消息继续传递
+    }  else 
+        return false;  // 允许消息传递
+}
+
 clientMessage('§lRunAway > Load InfiniteAuraRIP §b✔');
-clientMessage('§r§l 开源InfiniteAura(InfiniteAuraRIP)交流群 + 群聊756434157');
+clientMessage('§r§l 开源InfiniteAura(InfiniteAuraRIP) + 群聊756434157');
+clientMessage('§r§l UI原作者：github.com/MoYuanCN')
+clientMessage('§r§l 项目地址：https://github.com/MoYuanCN/RIP-PrivateMod/')
