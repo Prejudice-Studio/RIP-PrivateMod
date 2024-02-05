1dadfe653009d047a3843eb68ace89c491e0dac0
墨缘 <MoYuanCN@gmail.com>
2024-02-06 01:23:17 +0800

    更新 InfiniteAura - command.js

--- InfiniteAura - command.js
+++ InfiniteAura - command.js
@@ -232,9 +232,6 @@ function onSendChatMessageEvent(message) {
 }
 
 clientMessage('§lRunAway > Load InfiniteAuraRIP §b✔');
-clientMessage('§r§l开源InfiniteAura(InfiniteAuraRIP) + 群聊756434157');
-clientMessage('§r§lUI原作者：github.com/MoYuanCN QQ：1520349207 Email:MoYuanCN@gmail.com\n          LunarHax QQ：3588843609 Email：LunarHaxUI@outlook.com');
 clientMessage('§r§l项目地址：https://github.com/MoYuanCN/RIP-PrivateMod/');
-clientMessage('§r§l特别感谢:\nXxxGBRCxxX QQ：2938846249\n烟域 QQ：3595643051\n落日终归山海 QQ：2981864058\nAoux QQ：3511283331\nEcho QQ：1546348649\nCoe QQ：532998493\n墨影 QQ：3658044685');
 loadScript("bhop.js");
 clientMessage("基本信息显示仅在攻击时生效");
